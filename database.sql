81530bead0d2edb12eed79862fa14effa05c3568
JerwinIT <jerwinperiaIT@gmail.com>
2024-05-02 00:23:39 +0800

    First Commit

--- database.sql
+++
@@ -1,8 +0,0 @@
---system_credentials
-
-CREATE TABLE system_credentials(
-	user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN DEFAULT false
-);
--- src/index.js
+++ src/index.js
@@ -1,57 +1,13 @@
-<<<<<<< HEAD
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import BrowserRoutes from './BrowserRoutes';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+import BrowserRoutes from './BrowserRoutes'
 
-root.render(
-    <Provider store={store}>
-      <BrowserRoutes />
-    </Provider>
-
-);
-
-
-=======
-const express = require('express');
-const app = express();
-const { PORT, CLIENT_URL } = require('./constants');
-const cookieParser = require('cookie-parser')
-const passport = require('passport')
-const cors = require('cors')
-
-//import passport middleware
-require('./middlewares/passport-middleware')
-
-//init middleware
-app.use(express.json())
-app.use(cookieParser())
-
-app.use(cors({ origin: CLIENT_URL, credentials: true }))
-
-app.use(passport.initialize())
-
-//import routes
-const authenRoutes = require('./routes/authen')
-
-//Init routes
-app.use('/api', authenRoutes)
-
-
-//App start
-const appStart = () => {
-    try {
-        app.listen(PORT, () => {
-            console.log(`The app is running at http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-    }
-};
-
-appStart();
->>>>>>> da1f6e106d7870e4e2def3a580bdbff20b858b53
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRoutes />
+  </Provider>,
+  document.getElementById('root')
+)
